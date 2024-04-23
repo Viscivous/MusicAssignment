6a958cfcd6f92858214215387a31d398e999ae4c
Viscivous <94448336+Viscivous@users.noreply.github.com>
2024-04-23 11:59:39 +0100

    more buttons

--- Music.gd
+++ Music.gd
@@ -11,3 +11,43 @@ func _on_button_2_pressed():
 
 func _on_button_3_pressed():
 	$Piano/Button3/PianoB.play()
+
+
+func _on_button_4_pressed():
+	$Piano/Button4/PianoD.play()
+
+
+func _on_button_5_pressed():
+	$Piano/Button5/PianoE.play()
+
+
+func _on_button_6_pressed():
+	$Piano/Button6/PianoF.play()
+
+
+func _on_button_7_pressed():
+	$Piano/Button7/PianoG.play()
+
+
+func _on_button_8_pressed():
+	$Piano/Button8/PianoC2.play()
+
+
+func _on_button_9_pressed():
+	$Piano/Button9/PianoF2.play()
+
+
+func _on_button_10_pressed():
+	$Piano/Button10/PianoG2.play()
+
+
+func _on_button_11_pressed():
+	$Maracas/Button11/MaracaShake.play()
+
+
+func _on_button_12_pressed():
+	$Bongos/Button12/BongosL.play()
+
+
+func _on_button_13_pressed():
+	$Bongos/Button13/BongosR.play()
